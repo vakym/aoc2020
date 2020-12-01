6577a2a128fff9ede88d7c0321d264b6bd827130
Евгений Аввакумов <avakym92@gmail.com>
2020-12-01 08:58:01 +0300

    day 1 solve

---
+++ Day1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static string pathToInput;
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(pathToInput)
+                            .Select(line => int.Parse(line))
+                            .ToArray();
+            var values = FindValues();
+            Console.WriteLine(values.Item1*values.Item2);
+           
+
+            (int, int) FindValues()
+            {
+                for (var i = 0; i < input.Length; i++)
+                    for (var j = 0; j < input.Length - 1; j++)
+                    {
+                        if (i != j)
+                            if (input[i] + input[j] == 2020)
+                            {
+                                return (input[i],input[j]);
+                            }
+                    }
+                return (0, 0);
+            }
+        }
+    }
+}
