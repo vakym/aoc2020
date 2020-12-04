81d31281b119db22c2a05828336850f8ee15f3e0
vakym <aep@rifcom.ru>
2020-12-04 13:21:10 +0300

    day4 part one

---
+++ Day4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Input;
+
+namespace Day4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            InputReader.SessionKey = "your key";
+            var passports = (await InputReader.GetInput(4))
+                .Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+                .Select(data => data.Split(new[] {'\n', ' '},StringSplitOptions.RemoveEmptyEntries)
+                    .Select(str =>
+                    {
+                        var pair = str.Split(':');
+                        return (pair[0], pair[1]);
+                    }).ToDictionary(k => k.Item1, e => e.Item2))
+                .Count(IsValid);
+            Console.WriteLine(passports);
+        }
+
+        private static string[] reqFields =
+        {
+            "byr",
+            "iyr",
+            "eyr",
+            "hgt",
+            "hcl",
+            "ecl",
+            "pid"
+        };
+        private static bool IsValid(Dictionary<string, string> data)
+        {
+            foreach (var field in reqFields)
+                if (!data.ContainsKey(field))
+                    return false;
+            return true;
+        }
+    }
+
+    
+  
+
+}
